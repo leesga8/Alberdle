77b869a2c3b75c3625d26633b0424dd0287331a7
Albert Lee <leesga8@gmail.com>
2022-03-06 21:18:11 -0800

    8:00-9:00: research dom methods, event listeners

--- bertle.js
+++ bertle.js
@@ -2,31 +2,38 @@ const guessRow = document.querySelector('.row')
 const keyboard = document.querySelector('.letter')
 
 const keys = [
-  "A", 
-  "B", 
-  "C",
-  "D",
-  "E", 
-  "F", 
-  "G", 
-  "H",
-  "I", 
-  "J", 
-  "K", 
-  "L", 
-  "M", 
-  "N", 
+  "Q", 
+  "W", 
+  "E",
+  "R",
+  "T", 
+  "Y", 
+  "U", 
+  "I",
   "O", 
   "P", 
-  "Q",
-  "R", "S", "T", "U", "V", "W", "X", "Y", 
-  "Z",
+  "A", 
+  "S", 
+  "D", 
+  "F", 
+  "G", 
+  "H", 
+  "J",
+  "K", 
+  "L", 
   'ENTER',
+  "Z", 
+  "X", 
+  "C", 
+  "V", 
+  "B", 
+  "N", 
+  "M",
   'BACKSPACE',
 ]
 
-keys.forEach(key => {
+keys.forEach(element => {
   const buttonKey = document.createElement('button')
-  buttonKey.textContent = key
+  buttonKey.textContent = element
   keyboard.append(buttonKey)
 })
